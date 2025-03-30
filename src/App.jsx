0f19f570cc6440b8acda589ae75198195ef29b40
saritadipa34 <saritadipa34@gmail.com>
2025-03-30 09:03:52 +0530

    Removed rendering logic of RecipeBox and installed React-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { useState } from "react";
 
 const App=()=>{
   
   return(
-    <div className="min-h-screen px-[100px] py-[50px] w-screen bg-amber-300">
-      <h1 className="text-center font-bold text-4xl mb-2">FOOD RECIPE APP</h1>
+    <div className="min-h-screen px-[100px] py-[50px] w-screen bg-amber-400">
+      
       <Header />
       <Body />
+      
     </div>
   )
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-const Card=({itemName,itemImage,onClick})=>{
+const Card=({itemName,itemImage})=>{
    
     return(
         <div className="h-[270px] w-[250px] bg-white border flex flex-col items-center">
@@ -8,7 +8,7 @@ const Card=({itemName,itemImage,onClick})=>{
 <img className="h-[170px] w-full" src={itemImage} alt="image"/>
 </div>
 <p className="text-center my-4 font-bold">{itemName}</p>
-<Button onClick={onClick} className={"rounded-lg"} text={"Recipe"} />
+<Button    className={"rounded-lg"} text={"Recipe"} />
         </div>
     )
 }
